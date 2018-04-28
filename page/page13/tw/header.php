27168cfa3ed738cb6ae39abc4cf18591b9b983f1
Hung_Yi Lai <nximage23@gmail.com>
2018-04-28 12:01:00 +0100

    ..

---
+++ page/page13/tw/header.php
@@ -0,0 +1,15 @@
+<div class="wrapper">
+		<!-- header -->
+		<div id="header">
+			
+			<span><a href="../index.html">Nicky's Homemade Meals</a></span>
+			
+			<ul id="header-nav">
+				<li><a href="test.html">
+					<img src="../pics/about.png" /></a></li>
+				<li><a href="test.html">
+					<img src="../pics/contact.png" /></a></li>
+				<li><a href="test.html">
+					<img src="../pics/sitemap.png" /></a></li>
+			</ul>
+		</div>
